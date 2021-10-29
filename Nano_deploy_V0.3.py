ab35ad755dc99a56c436305131884707033fa49c
Poornaditya Mishra <42487913+poornaditya1@users.noreply.github.com>
2021-10-29 14:42:13 +0530

    Add functionalities

--- Nano_deploy_V0.3.py
+++ Nano_deploy_V0.3.py
@@ -3,6 +3,12 @@ import os
 from playsound import playsound
 import speech_recognition as sr
 import wikipedia
+import pyjokes
+import datetime
+from gnewsclient import gnewsclient
+import requests
+from urllib.request import urlopen
+import json
 
 import os
 import torch
@@ -35,10 +41,11 @@ intro = '''Hello, I am Maya 1.0 created at the Department of Automation and Robo
 
 department_intro = '''I was created in the Department of Automation and Robotics Engineering in. KLE Technological University. Mr. Arun Giriyapur is the head of the department. The department prepares students to work efficiently with industrial robots and automation systems. The main areas of focus are cognitive intelligence, electronics, robotics and mechanical systems. Students integrate electronics with mechanical systems and programmable controllers and explore alternative trade-offs in the process of problem-solving.'''
 
+name = ''
 
 use_own_model = False
 
-model_name_or_path = "twmkn9/albert-base-v2-squad2"#"ktrapeznikov/albert-xlarge-v2-squad-v2"
+model_name_or_path = "elgeish/cs224n-squad2.0-albert-large-v2" #"twmkn9/albert-base-v2-squad2"#"ktrapeznikov/albert-xlarge-v2-squad-v2"
 output_dir = ""
 
 n_best_size = 1
@@ -57,7 +64,7 @@ tokenizer = tokenizer_class.from_pretrained(
     model_name_or_path, do_lower_case=True)
 model = model_class.from_pretrained(model_name_or_path, config=config)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda")
 
 model.to(device)
 
@@ -151,6 +158,7 @@ def ask():
 
     # print("What question do you want me to answer")
     ques = listen(3)
+    print("Question: ",ques)
     # ques = input()
     prediction = run_prediction(ques, context)
     print(prediction["0"])
@@ -170,12 +178,13 @@ def ask():
 
 
 def speak(text):
-    tts = gTTS(text,lang = 'en',tld='co.in',slow=False)
-    tts.save('answer.mp3')
-    sound_file = 'answer.mp3'
-    playsound(sound_file)
-    print("Maya has spoken")
-    os.remove('answer.mp3')
+    #tts = gTTS(text,lang = 'en',tld='co.in',slow=False)
+    #tts.save('answer.mp3')
+    #sound_file = 'answer.mp3'
+    #playsound(sound_file)
+    #print("Maya has spoken")
+    #os.remove('answer.mp3')
+    print(text)
 
 def split_into_sentences(text):
     text = " " + text + "  "
@@ -235,8 +244,17 @@ def ask_wiki(str):
     print("Searching...")
     speak("Hang on a minute")
     print("Topic = ",topic)
-    page_data = wikipedia.page(topic)
-    answer = page_data.content
+    try:
+        page_data = wikipedia.page(topic, auto_suggest=False)
+        answer = page_data.content
+    except wikipedia.DisambiguationError as e:
+        speak("I am confused between the following options")
+        for opt in e.options:
+            speak(opt)
+            print(opt)
+        topic = listen(5)
+        sen = ask_wiki(topic)
+        return sen
     if answer is None:
         return -1
     else:
@@ -245,44 +263,95 @@ def ask_wiki(str):
 
 def listen(a=None):
     r = sr.Recognizer()
-    mic = sr.Microphone(device_index=9)
+    mic = sr.Microphone(device_index=26)
     print("Be ready to speak")
     with mic as source:
-        r.adjust_for_ambient_noise(source)
+        r.adjust_for_ambient_noise(source, duration = 0.5)
         if a is 0:
             speak("What should i do for you")
         elif a is 1:
             speak("Do you want to know more")
         elif a is 3:
             speak("What question do you want me to answer")
+            time.sleep(2)
         elif a is 4:
             speak("Please tell me the answer")
+        elif a is 5:
+            speak("Please select your choice")
+        elif a is 6:
+            speak("What is your name?")
+        elif a is 7:
+            speak("Please tell me your topic of interest")
         elif a is None:
             pass
 
         print("START SPEAKING NOW")
-        audio = r.listen(source)
+        #audio = r.listen(source)
 
-    str = r.recognize_google(audio)
-    return str
+    #str = r.recognize_google(audio)
+    string = input()
+    return string
+
+def greet(name):
+    cur_time = time.localtime(time.time())
+    if cur_time.tm_hour<12:
+        greeting = "Good morning "
+    elif cur_time.tm_hour<17:
+        greeting = "Good afternoon "
+    else:
+        greeting = "Good evening "
+    return greeting+name
 
 speak("Maya is online and ready to comply")
+name = listen(6)
+speak(greet(name))
 while True:
 
     # print("What do you want me to do")
-    str = listen(0)
+    command = listen(0)
     # str = input()
-    str = str.split()
-    if "introduce" in str or "introduction" in str:
-        if "department" in str:
+    str_q = command.split()
+    if "how are you" in command:
+        speak("I'm doing great")
+    elif "name" in str_q:
+        speak("My name is Maya")    
+    elif "introduce" in str_q or "introduction" in str_q:
+        if "department" in str_q:
             department_introduction()
         else:
             introduction()
-    elif "about" in str:
+    elif "joke" in str_q:
+        jk = pyjokes.get_joke("en")
+        speak(jk)
+
+    elif 'news' in str_q:
+        n_topic = listen(7)
+        client = gnewsclient.NewsClient(language = 'english', location = 'india', topic = n_topic, max_results = 3)
+        news = client.get_news()
+        speak("The headlines are as follows")
+        for item in news:
+            speak(item['title'])
+    elif "weather" in str_q:
+        api_k = "4e68859684a3c41718d5aaa517641d1f"
+        base_url = "http://api.openweathermap.org/data/2.5/weather?"
+        city = "Hubli"
+        complete_url = base_url + "appid=" + api_k + "&q=" + city
+        response = requests.get(complete_url)
+        x = response.json()
+        if x["cod"] != "404":
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_pressure = y["pressure"]
+            current_humidiy = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            speak(" Temperature (in kelvin unit) = " +str(current_temperature)+"\n atmospheric pressure (in hPa unit) ="+str(current_pressure) +"\n humidity (in percentage) = " +str(current_humidiy) +"\n description = " +str(weather_description))
+
+    elif "about" in str_q:
         #speak('What would you like to know about')
         #topic = listen()
         # sen = ask_wiki(topic)
-        sen = ask_wiki(str)
+        sen = ask_wiki(str_q)
         if sen == -1:
             speak("Sorry, I don't know the answer")
         else:
@@ -300,8 +369,13 @@ while True:
                     b+=5
                 else:
                     flag = False
-    elif "ask" in str or "answer" in str:
+    elif "ask" in str_q or "answer" in str_q:
         ask()
-    elif "exit" in str:
-        speak("Thank you")
+    elif "exit" in str_q or "bye" in str_q:
+        cont = "thank you "+name
+        if time.localtime(time.time()).tm_hour < 20:
+            speak(cont+". Have a great day")
+        else:
+            speak(cont+". Good night")
+        name =''
         break
